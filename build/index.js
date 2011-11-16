9004de77f5883adceac5286094f75df0087eb861
Matt Baker <matthew.reiss.baker@gmail.com>
2011-11-15 22:48:47 -0500

    cleaned

--- build/index.js
+++ build/index.js
@@ -8,16 +8,12 @@ process.stdin.resume();
 
 process.stdin.setEncoding('ascii');
 
-process.stdin.on('data', function(data) {
+process.stdin.on('data', function(chunk) {
   var lastTest, test, tests, _i, _len;
-  input += data;
+  input += chunk;
   tests = input.split('\n');
   lastTest = tests[tests.length - 1];
-  if (lastTest.charAt(lastTest.length - 1) === '\n') {
-    input = '';
-  } else {
-    input = tests.pop();
-  }
+  input = lastTest.charAt(lastTest.length - 1) === '\n' ? '' : tests.pop();
   for (_i = 0, _len = tests.length; _i < _len; _i++) {
     test = tests[_i];
     console.log(hideCreditCards(test));
--- index.coffee
+++ index.coffee
@@ -5,14 +5,11 @@ input = ''
 
 process.stdin.resume()
 process.stdin.setEncoding 'ascii'
-process.stdin.on 'data', (data) ->
-	input += data
+process.stdin.on 'data', (chunk) ->
+	input += chunk
 	tests = input.split '\n'
 	lastTest = tests[tests.length - 1]
-	if lastTest.charAt(lastTest.length - 1) == '\n'
-		input = ''
-	else
-		input = tests.pop()
+	input = if lastTest.charAt(lastTest.length - 1) == '\n' then '' else tests.pop()
 	
 	for test in tests
 		console.log hideCreditCards test
